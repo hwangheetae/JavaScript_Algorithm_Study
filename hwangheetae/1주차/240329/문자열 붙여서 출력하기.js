41fbf0cbe5ced53a52a6e96b1fc604a5a30d6cb9
hwangheetae <tejaia@naver.com>
2024-03-29 11:54:36 +0900

    feat: PGS 문자열 붙여서 출력하기

---
+++ hwangheetae/1주차/240329/문자열 붙여서 출력하기.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  console.log(str1 + str2);
+});

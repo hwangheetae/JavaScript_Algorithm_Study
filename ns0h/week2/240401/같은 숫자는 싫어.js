7b052825fe3aca21f77dc67c5810beb537a65325
NS0H <bory1610@gmail.com>
2024-04-01 18:59:01 +0900

    comment: 다른 사람 풀이 추가

--- ns0h/week2/240401/같은 숫자는 싫어.js
+++ ns0h/week2/240401/같은 숫자는 싫어.js
@@ -28,3 +28,8 @@ function solution(arr) {
 
 console.log(solution([1,1,3,3,0,1,1])); // [1,3,0,1]
 console.log(solution([4,4,4,3,3])); // [4,3]
+
+/*
+다른 사람 풀이
+return arr.filter((val,index) => val != arr[index+1]);
+*/

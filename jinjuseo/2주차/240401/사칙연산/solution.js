9372586179ef6ee65a31a4abc10f455564a9a8ea
Jinju Seo <wjbbdq1223@naver.com>
2024-04-01 22:12:35 +0900

    feat : PGS 사칙연산(입문)

---
+++ jinjuseo/2주차/240401/사칙연산/solution.js
@@ -0,0 +1,18 @@
+function solution(num1, num2) {
+  var answer = num1 + num2;
+  return answer;
+}
+
+function solution(num1, num2) {
+  var answer = num1 - num2;
+  return answer;
+}
+function solution(num1, num2) {
+  var answer = num1 * num2;
+  return answer;
+}
+
+function solution(num1, num2) {
+  var answer = Math.floor(num1 / num2);
+  return answer;
+}

b9507259fb2003ae7b9fd7883625472c07b82e7f
hwangheetae <tejaia@naver.com>
2024-04-12 10:48:27 +0900

    fix: BOJ 2751-수 정렬하기 2

--- hwangheetae/3주차/240412/수 정렬하기 2 .js
+++ hwangheetae/3주차/240412/수 정렬하기 2 .js
@@ -1,4 +1,12 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().split(" ");
-const [a, b] = input.map((s) => parseInt(s, 10));
-console.log(a + b);
+let input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const [N, ...numList] = input.map((e) => Number(e));
+    const result = numList.sort((a, b) => a - b);
+    console.log(result.join("\n"));
+  });

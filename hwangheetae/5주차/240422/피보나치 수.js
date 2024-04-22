32f3c58c49210d7ddd218fb74ccdf4c5835da224
hwangheetae <62371559+hwangheetae@users.noreply.github.com>
2024-04-22 22:54:10 +0900

    docs: 주석추가

--- hwangheetae/5주차/240422/피보나치 수.js
+++ hwangheetae/5주차/240422/피보나치 수.js
@@ -8,3 +8,17 @@ for (let i = 2; i < 100001; i++) {
 function solution(n) {
   return memo[n];
 }
+
+
+//오답
+// const memo =[0]*100001
+// memo[0]=0
+// memo[1]=1
+
+// for(let i = 2;i<100001;i++){
+//     memo[i] = memo[i-1]+memo[i-2]
+// }
+// console.log(memo)
+// function solution(n) {
+//     return memo[n]
+// }
